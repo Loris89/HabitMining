1ddd36e60293a26073326036d2fb981dc8293096
Loris89 <loristefano@hotmail.it>
2016-09-21 08:43:31 +0200

    ulrs changed

--- app/src/main/java/stefano/loris/habitmining/app/AppConfig.java
+++ app/src/main/java/stefano/loris/habitmining/app/AppConfig.java
@@ -2,11 +2,11 @@ package stefano.loris.habitmining.app;
 
 public class AppConfig {
 
-    public static String URL_LOGIN = "http://www.sapienza.esy.es/checkPhone.php";
+    public static String URL_LOGIN = "http://habitmining.altervista.org/checkPhone.php";
 
     public static String URL_REGISTER = "http://www.sapienza.esy.es/registerPhone.php";
 
-    public static String URL_GET_ACTIVITIES = "http://www.sapienza.esy.es/getWeightedPhoneActivities.php";
+    public static String URL_GET_ACTIVITIES = "http://habitmining.altervista.org/getWeightedPhoneActivities.php";
 
-    public static String URL_PUT_ACTIVITY = "http://www.sapienza.esy.es/insertAction.php";
+    public static String URL_PUT_ACTIVITY = "http://habitmining.altervista.org/insertAction.php";
 }
